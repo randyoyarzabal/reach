89abf7795d5ca4c3debff12ce5494cab2e0ecad9
Randy E. Oyarzabal <32100282+randyoyarzabal@users.noreply.github.com>
2018-10-25 09:05:01 -0500

    Added optional helper BASH lib.

---
+++ extras/reach_bash-lib.sh
@@ -0,0 +1,178 @@
+#!/usr/bin/env bash
+# This is an OPTIONAL BASH library for use with Reach. Feel free to add aliases, functions etc.
+# To use this, just source in your start_up script (i.e. profile or bashrc)
+
+# A collection of utility environment vars, aliases and functions for Reach etc.
+#
+# Dev notes:
+#    - Add "${@:N}" to the end of a function for dynamic parameters to be passed. Where N represents the optional parameter.
+#    - Single quotes wont expand vars unless used in an alias. Use double quotes instead.
+
+# ALIAS DEFINITIONS
+############################
+
+shopt -s expand_aliases
+
+# Define the reach location here
+REACH_DIR='/Users/m173404/dev/repos/github/reach'
+REACH_BASH_LIB="$REACH_DIR/extras/reach_bash-lib.sh"
+REACH_TOOL="$REACH_DIR/reach.py"
+
+alias reach='$REACH_TOOL'
+
+# Define any special aliases here
+
+# Example aliases:
+# alias reach-linux='$REACH_TOOL -f TYPE=Linux'
+
+# GENERAL UTILITY FUNCTIONS
+############################
+
+function reach.search {
+   if [ -z "$1" ] || [ -z "$2" ] || [ "$1" == "-?" ]; then
+      echo "Find a string in the result of a command against the non-SIP fleet."
+      echo "Optionally pass -all to search entire fleet (active only)."
+      echo "Usage: $FUNCNAME <command> <search string> [-all]";
+      return;
+   fi
+   __timer start
+
+   echo "Executing \"$1\" against selected hosts.  Searching for: \"$2\"";
+
+   reach -c "$1" -s "$2"'|$NF' -r 'Found in Host: $HF_1 ($HF_3)|Not Found in Host: $HF_1 ($HF_3)' "${@:3}" | grep --color=never Found;
+
+   __timer end
+}
+
+# HELPER FUNCTIONS
+############################
+function reach.edit_library() {
+   if [ "$1" == "-?" ]; then
+      echo "Edit the utility library."
+      echo "Usage: $FUNCNAME"
+      return;
+   fi
+   date=$(stat -c %y "$REACH_BASH_LIB");
+   vim $REACH_BASH_LIB;
+   date2=$(stat -c %y "$REACH_BASH_LIB");
+
+   # check if the file was actually modified before reloading
+   if [[ $date2 != $date ]]; then
+     echo "REACH BASH library was modified."
+     web.reload_library
+   fi
+}
+
+function reach.reload_library() {
+   if [ "$1" == "-?" ]; then
+      echo "Reload this utility library."
+      echo "Usage: $FUNCNAME"
+      return;
+   fi
+   . $REACH_BASH_LIB
+   echo "REACH BASH library was reloaded."
+}
+
+function __timer () {
+   # Usage: __timer <start | end>
+   if [ "$1" = "start" ]; then SECONDS=0; return; fi
+   if [ "$1" = "end" ]; then echo "Task took: "`date +%T -d "1/1 + $SECONDS sec"`; fi
+}
+
+function __lower() {
+   # Usage: __lower <string>
+   valStr=$1
+   valStr=`echo $valStr | awk '{print tolower($0)}'`
+   echo $valStr
+}
+
+function __upper() {
+   # Usage: __upper <string>
+   valStr=$1
+   valStr=`echo $valStr | awk '{print toupper($0)}'`
+   echo $valStr
+}
+
+function __proper() {
+   # Usage: __proper <string>
+   sed 's/.*/\L&/; s/[a-z]*/\u&/g' <<<"$1"
+}
+
+function __isvalid_ip() {
+   # Usage: __isValid_ip
+   local  ip=$1
+   local  stat=1
+
+   if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+       OIFS=$IFS
+       IFS='.'
+       ip=($ip)
+       IFS=$OIFS
+       [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+           && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+       stat=$?
+   fi
+   return $stat
+}
+
+function __begin {
+   # Linux implementation of Cisco's "begin"
+   cat | sed -n "/$1/,\$p"
+}
+alias be='__begin'
+
+function __show_progress() {
+   # Usage: __show_progress <msg> <command> <output_variable>
+   MSG_LENGTH=$(echo -n $1 | wc -m)
+   # Create a random file to hold output
+   tmp_file="/tmp/._`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1`"
+
+   # Process the command in the background until it returns (saving the output to the temp file.
+   #   In the meantime, keep printing the spinner chars.
+   read < <( eval "$2" > $tmp_file & echo $! ); printf "$1"; __spinner $REPLY;
+
+   # Clear the message in-place
+   start=1
+   end=$MSG_LENGTH
+
+   # Move the cursor to the left
+   for ((i=$start; i<=$end; i++)); do printf "$KEYS_LEFT"; done
+
+   # Blank the message
+   for ((i=$start; i<=$end; i++)); do printf " "; done
+
+   # Reposition the cursor to the beginning before any other writes to the screen
+   for ((i=$start; i<=$end; i++)); do printf "$KEYS_LEFT"; done
+
+   # Save output to 3rd parameter variable
+   eval "$3='`cat $tmp_file`'"
+
+   # Display the command output to the screen
+   # cat $tmp_file
+
+   # Destroy / delete the temp file
+   rm -rf $tmp_file
+}
+
+function __spinner() {
+    local pid=$1
+    local delay=0.75
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
+function __ask_yes_or_no() {
+    read -p "$1 ([y]es or [N]o): "
+    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
+        y|yes) echo "yes" ;;
+        *)     echo "no" ;;
+    esac
+}
+
